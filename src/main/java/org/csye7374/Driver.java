04a6f227cca3ff479583a8b13bf210cd916a1823
tanaysaxena97 <tanaysaxena97@gmail.com>
2023-04-26 18:30:55 -0400

    added code to demo factory, builder and singleton

--- src/main/java/org/csye7374/Driver.java
+++ src/main/java/org/csye7374/Driver.java
@@ -1,11 +1,20 @@
 package org.csye7374;
 
-import org.csye7374.Item.Inventory;
-import org.csye7374.Item.Item;
+import org.csye7374.Item.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Driver {
     public static void main(String[] args) {
         System.out.println("App started ....");
+        demoState();
+        demoItemSingletonFactoryAndBuilder();
+        System.out.println("App exit ....");
+    }
+
+    private static void demoState() {
+        System.out.println("-----------------------State-----------------------");
         //State Pattern change the State if Item is not available
         Item item = new Item( 10.99,"book");
         Item item1= new Item( 10.99,"pen");
@@ -14,12 +23,28 @@ public class Driver {
 
         inventory.addItem(item);
 
-
         if (inventory.isItemAvailable(item1)) {
             System.out.println(item1.getName() + " is available for purchase.");
         } else {
             System.out.println(item1.getName() + " is out of stock.");
         }
+        System.out.println("-----------------------END-----------------------");
+    }
+
+    private static void demoItemSingletonFactoryAndBuilder() {
+        System.out.println("-----------------------Factory, Singleton and Builder-----------------------");
+        List<ItemAPI> items = new ArrayList<>();
+        ItemBuilder builder = new ItemBuilder();
+        builder.setName("Apple (x6)").setPrice(3.99);
+        items.add(ItemFactoryEagerSingleton.getInstance().getObject(builder));
+        builder.setName("Eggs 18").setPrice(5.49);
+        items.add(ItemFactoryEagerSingleton.getInstance().getObject(builder));
+        builder.setName("Gatorade (x6)").setPrice(8.49);
+        items.add(ItemFactoryEagerSingleton.getInstance().getObject(builder));
+        builder.setName("Chocolate Box").setPrice(10.0);
+        items.add(ItemFactoryEagerSingleton.getInstance().getObject(builder));
+        items.stream().forEach(i -> {System.out.println(i);});
+        System.out.println("-----------------------END-----------------------");
     }
 }
 
